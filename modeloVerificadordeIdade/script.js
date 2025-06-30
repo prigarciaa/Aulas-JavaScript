5c9ceca3db6bf7989138fc24b4e1f1d35142285a
prigarciaa <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>
2025-06-30 17:00:20 -0300

    start commit

---
+++ modeloVerificadordeIdade/script.js
@@ -0,0 +1,13 @@
+function verificar() { 
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('textano')
+    var res = document.querySelector('div#res')
+    if (fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+
+    }  
+}
