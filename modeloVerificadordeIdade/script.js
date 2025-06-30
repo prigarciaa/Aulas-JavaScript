92b2e51cfad85464a0d31497f9e52aa85be69193
prigarciaa <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>
2025-06-30 19:24:09 -0300

    colocando idade e foto

--- modeloVerificadordeIdade/script.js
+++ modeloVerificadordeIdade/script.js
@@ -8,6 +8,44 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'criançaMenino.png')
+            } else if (idade < 21) {
+                // Jovem 
+                img.setAttribute('src', 'jovemHomem.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultoHomem.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosoHomem.png')
+            }      
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'criançamulher.png')
+            } else if (idade < 21) {
+                // Jovem 
+                img.setAttribute('src', 'jovemMulher.png')
+            } else if (idade < 50) {
+                // Adulto
+                img.setAttribute('src', 'adultoMulher.png')
+            } else {
+                // Idoso
+                img.setAttribute('src', 'idosa.png')
+            }      
+    
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }  
+
 }
