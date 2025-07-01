aae1aa7c469c462afa3d0e9fb5d843140867d594
prigarciaa <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>
2025-07-01 15:58:42 -0300

    criando a function

---
+++ modeloVamosContar/script.js
@@ -0,0 +1,20 @@
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: '
+        let i = Number (inicio.value)
+        let f = Number (fim.value)
+        let p = Number (passo.value)
+
+        for(let c = i; c <= f; c += p) {
+            res.innerHTML += `${c}`
+        } 
+
+    } 
+
+}
