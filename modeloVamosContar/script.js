67e3bc44241f3c65dc5ddadf6de039f8edc568a5
prigarciaa <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>
2025-07-01 16:52:22 -0300

    add emoji

--- modeloVamosContar/script.js
+++ modeloVamosContar/script.js
@@ -5,17 +5,30 @@ function contar() {
     let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        window.alert('[ERRO] Faltam dados!')
+        res.innerHTML = 'Impossível contar!'
+        //window.alert('[ERRO] Faltam dados!')
     } else {
-        res.innerHTML = 'Contando: '
+        res.innerHTML = 'Contando: <br>'
         let i = Number (inicio.value)
         let f = Number (fim.value)
         let p = Number (passo.value)
-
-        for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c}`
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSP 1')
+            p = 1
+        }
+        if (i < f) {
+            // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        } else {
+            // Contagem recressiva
+            for(let c = i; c >= f; c -= p) {
+                res.innerHTML += `${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1F3C1}`
         } 
 
     } 
 
-}
