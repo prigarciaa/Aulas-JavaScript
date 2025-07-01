7b1a704370d5f4de18b4e489fb740ce8d761f4ff
prigarciaa <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>
2025-07-01 19:09:35 -0300

    add limpa tabela

--- modeloTabuada/script.js
+++ modeloTabuada/script.js
@@ -1,17 +1,19 @@
 function tabuada() {
     let num = document.getElementById('txtn')
-    let tab = document.getElementById('setab')
+    let tab = document.getElementById('seltab')
     if (num.value.length == 0) {
         window.alert('Por favor, digite um número!')    
     } else {
         let n = Number(num.value)
         let c = 1
+        tab.innerHTML = '' // Limpa o conteúdo da tabela
         while (c <= 10) {
             let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
         }
     }
-
-
 
 
 }
