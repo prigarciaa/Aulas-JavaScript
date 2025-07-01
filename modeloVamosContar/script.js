f69669514205ba33d75900e93c1519bdf06172fb
prigarciaa <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>
2025-07-01 16:06:02 -0300

    atualizando body e section

--- modeloVamosContar/script.js
+++ modeloVamosContar/script.js
@@ -2,6 +2,7 @@ function contar() {
     let inicio = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         window.alert('[ERRO] Faltam dados!')
