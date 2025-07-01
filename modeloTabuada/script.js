35faec5bf1875898825f968ff2ffdedbb9f15ab3
prigarciaa <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>
2025-07-01 18:31:12 -0300

    add function js

---
+++ modeloTabuada/script.js
@@ -0,0 +1,17 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('setab')
+    if (num.value.length == 0) {
+        window.alert('Por favor, digite um número!')    
+    } else {
+        let n = Number(num.value)
+        let c = 1
+        while (c <= 10) {
+            let item = document.createElement('option')
+        }
+    }
+
+
+
+
+}
